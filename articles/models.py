13de60b51eb732e6d8229e359f71dce5b2431b6d
Stelour <stelourgg@gmail.com>
2026-05-23 17:24:22 +0500

    first commit

---
+++ articles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    thumbnail = models.ImageField(upload_to = 'articles/', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def snipper(self):
+        return self.body[:50] + '...'
